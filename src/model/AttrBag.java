c8d08452d3308cb9310e362ec40d113fae048fed
Martin Delzant <delzantmartin@gmail.com>
2015-01-30 16:43:55 +0100

    Modif constructeur AttrBag

--- src/model/AttrBag.java
+++ src/model/AttrBag.java
@@ -17,12 +17,12 @@ public class AttrBag {
 	private int shar;
 	
 	//Constructor (with ratings on 10)
-	AttrBag(int attr, int sinc, int intel, int fun, int amb, int shar){
+	public AttrBag(int attr, int sinc, int intel, int fun, int amb, int shar){
 		this(attr, sinc, intel, fun, amb, shar, false);
 	}
 	
 	//Constructor (with ratings on 100 if boolean is true)
-	AttrBag(int attr, int sinc, int intel, int fun, int amb, int shar, boolean on100){
+	public AttrBag(int attr, int sinc, int intel, int fun, int amb, int shar, boolean on100){
 		if(on100 || attr>10 || sinc>10 || intel>10 || fun>10 || amb>10 || shar>10){
 			this.attr = (int) (meanRating*(((double)attr) /100.));
 			this.sinc = (int) (meanRating*(((double)sinc) /100.));
