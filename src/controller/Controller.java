f8ec7df514574a73a49624433d854469840e25a9
zaccharieramzi <zaccharie.ramzi@gmail.com>
2015-02-05 18:25:04 +0100

    Changement des paramètres des méthoddes : on ne fait plus de String gender mais des Sex sex. C'est plus cohérent avec ce qu' a fait Martin.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import model.AttrBag;
+import model.constants.Sex;
 
 public class Controller implements ControllerInterface {
 	
@@ -20,42 +21,42 @@ public class Controller implements ControllerInterface {
 	}
 
 	//Renvoie une liste des goûts préférés des hommes ou des femmes de cet âge.
-	public ArrayList<String> getPreferredTaste(String gender) {
+	public ArrayList<String> getPreferredTaste(Sex sex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	//Renvoie le pourcentage de match à la fin de la nuit pour les hommes (ou les femmes) de cet âge
-	//how many yes halfway
-	public double getMatchPercentage(String gender) {
-		// TODO Auto-generated method stub
-		int totalNumberOfPeople=0;
-		int numberOfPeopleThatMatched=0;
+	//how many yes halfway. Pour un homme mettre Male
+	public double getMatchPercentage(Sex sex) {
+		
+		int totalNumberOfPeople=loader.nbrPersons(age, sex);
+		int numberOfPeopleThatMatched=loader.personsThatMatched(age, sex);
 		return totalNumberOfPeople/numberOfPeopleThatMatched;
 	}
 
 	//Renvoie le nombre de yes que la personne pense avoir obtenu (réponse supérieure à 5) dans la nuit (si on ne fait pas halfway).
-	public int getAmountOfYess(String gender) {
-		// TODO Auto-generated method stub
+	public int getAmountOfYess(Sex sex) {
+		int[] array = loader.predictionRateYesAnswers(age, sex);
 		
 		return 0;
 	}
 
 	//renvoie un enum (ChangementDePerception) qui indique combien la perception a changé
-	public ChangementDePerception hasPerceptionOfOhtersChanged(String gender) {
+	public ChangementDePerception hasPerceptionOfOhtersChanged(Sex sex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	//idem
-	public ChangementDePerception hasSelfPerceptionChanged(String gender) {
+	public ChangementDePerception hasSelfPerceptionChanged(Sex sex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	//renvoie un enum(Satisfaction) qui donne la satisfaction sur toute la nuit.
 
-	public Satisfaction getSatisfactionOverall(String gender) {
+	public Satisfaction getSatisfactionOverall(Sex sex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -67,27 +68,27 @@ public class Controller implements ControllerInterface {
 	}
 
 	//Ouvrir une fenêtre lorsqu'on clique sur les goûts d'un genre particulier
-	public void handleClickOnTaste(String gender) {
+	public void handleClickOnTaste(Sex sex) {
 		// TODO Auto-generated method stub
 
 	}
 
 	//Renvoie les vecteurs correspondants aux informations pour la fenêtre detailedView
-	public HashMap<String, double[]> getTaste(String gender) {
+	public HashMap<String, double[]> getTaste(Sex sex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	//ouvre la fenêtre correspondant l’évolution des différentes perceptions (de soi même, que les autres ont de soi même, de celle de son genre et de celle du genre opposé, recherche)
 
-	public void handleClickOnPerception(String gender, TypeDePerception type,
+	public void handleClickOnPerception(Sex sex, TypeDePerception type,
 			Step step) {
 		// TODO Auto-generated method stub
 
 	}
 
 	//renvoie les différentes notes aux questions de perception 
-	public HashMap<String, AttrBag> getPerception(String gender,
+	public HashMap<String, AttrBag> getPerception(Sex sex,
 			TypeDePerception type, Step step) {
 		// TODO Auto-generated method stub
 		return null;
--- src/controller/ControllerInterface.java
+++ src/controller/ControllerInterface.java
@@ -4,19 +4,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import model.AttrBag;
+import model.constants.Sex;
 
 public interface ControllerInterface {
 	
-	ArrayList<String> getPreferredTaste(String gender);
-	double getMatchPercentage (String gender);
-	int getAmountOfYess(String gender);
-	ChangementDePerception hasPerceptionOfOhtersChanged(String gender);
-	ChangementDePerception hasSelfPerceptionChanged(String gender);
-	Satisfaction getSatisfactionOverall(String gender);
+	ArrayList<String> getPreferredTaste(Sex sex);
+	double getMatchPercentage (Sex sex);
+	int getAmountOfYess(Sex sex);
+	ChangementDePerception hasPerceptionOfOhtersChanged(Sex sex);
+	ChangementDePerception hasSelfPerceptionChanged(Sex sex);
+	Satisfaction getSatisfactionOverall(Sex sex);
 	void setAge(int age);
-	void handleClickOnTaste(String gender);
-	HashMap<String,double[]> getTaste(String gender);
-	void handleClickOnPerception(String gender, TypeDePerception type, Step step);
-	HashMap<String,AttrBag> getPerception(String gender, TypeDePerception type, Step step);
+	void handleClickOnTaste(Sex sex);
+	HashMap<String,double[]> getTaste(Sex sex);
+	void handleClickOnPerception(Sex sex, TypeDePerception type, Step step);
+	HashMap<String,AttrBag> getPerception(Sex sex, TypeDePerception type, Step step);
 
 }
