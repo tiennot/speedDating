325f7c4b2c540461e835da6fe869b352f352a24d
Martin Delzant <delzantmartin@gmail.com>
2015-02-07 14:59:47 +0100

    New Class for the Career

---
+++ src/model/constants/Career.java
@@ -0,0 +1,40 @@
+package model.constants;
+
+public class Career {
+	private int career ;
+
+	public Career(int career) {
+		super();
+		this.career = career;
+	}
+
+	public int getCareer() {
+		return career;
+	}
+
+	public void setCareer(int career) {
+		this.career = career;
+	}
+	
+	//Constants 
+	public static final Career LAWYER = new Career((int) 1);
+	public static final Career ACADEMIC_RESEARCH = new Career((int) 1);
+	public static final Career PSYCHOLOGIST = new Career((int) 1);
+	public static final Career DOCTOR_MEDICINE = new Career((int) 1);
+	public static final Career ENGINEER = new Career((int) 1);
+	public static final Career CREATIVE_ARTS_ENTERTAINEMENT = new Career((int) 1);
+	public static final Career BANKING_CONSULTING_CEO_ENTREPRENEUR = new Career((int) 1);
+	public static final Career REAL_ESTATE = new Career((int) 1);
+	public static final Career INTERNATIONAL_HUMANITARIAN_AFFAIRS = new Career((int) 1);
+	public static final Career UNDECIDED = new Career((int) 1);
+	public static final Career SOCIAL_WORK = new Career((int) 1);
+	public static final Career SPEECH_PATHOLOGY = new Career((int) 1);
+	public static final Career POLITICS = new Career((int) 1);
+	public static final Career PRO_SPORTS_ATHLETICS = new Career((int) 1);
+	public static final Career OTHER = new Career((int) 1);
+	public static final Career JOURNALISM = new Career((int) 1);
+	public static final Career ARCHITECTURE = new Career((int) 1);
+	
+	
+
+}
