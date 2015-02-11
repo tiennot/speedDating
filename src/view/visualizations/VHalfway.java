362966c2b3022230c4635bd8542e1699936fe7e1
tiennot <camille.tiennot@telecom-paristech.fr>
2015-02-11 14:29:38 +0100

    Added VHalfway to GUI

---
+++ src/view/visualizations/VHalfway.java
@@ -0,0 +1,17 @@
+package view.visualizations;
+
+import processing.core.PApplet;
+import view.DetailWindow;
+
+public class VHalfway extends Visualization {
+
+	public VHalfway(PApplet p, DetailWindow parent) {
+		super(p, parent);
+	}
+
+	@Override
+	public void draw() {
+		
+	}
+
+}
